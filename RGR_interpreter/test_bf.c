5deb50ebc7fe9ed61a8e4219f2ab570866d7cf2d
M4llone <mihail.kazakov07@mail.ru>
2026-05-29 10:58:43 +0700

    Внес изменения в test_bf.c

--- RGR_interpreter/test_bf.c
+++ RGR_interpreter/test_bf.c
@@ -9,26 +9,26 @@
 
 static void test_check_data_correct(void **state) {
     (void) state;
-    const char *good_code = "+++++[>+++++++<-].";
-    int result = check_data(good_code);
+    const char *correct= "+++++[>+++++++<-].";
+    int result = check_data(correct);
     assert_int_equal(result, 1); 
 }
 
 static void test_check_data_unmatched_close(void **state) {
     (void) state;
-    const char *bad_code = "+++++>-<-]";
-    int result = check_data(bad_code);
+    const char *uncorrect = "+++++>-<-]";
+    int result = check_data(uncorrect);
     assert_int_equal(result, 0); 
 }
 
 static void test_check_data_unmatched_open(void **state) {
     (void) state;
-    const char *bad_code = "+++++[>++++<-";
-    int result = check_data(bad_code);
+    const char *uncorrect = "+++++[>++++<-";
+    int result = check_data(uncorrect);
     assert_int_equal(result, 0); 
 }
 
-static void test_clear_tape(void **state) {
+static void test_clear(void **state) {
     (void) state;
     int size = 10;
     char *tape = (char*)malloc(size * sizeof(char));
@@ -46,7 +46,7 @@ static void test_clear_tape(void **state) {
     free(tape);
 }
 
-static void test_interpreter_success_simple(void **state) {
+static void test_interpreter(void **state) {
     (void) state;
     int tape_size = 10;
     char *tape = (char*)calloc(tape_size, sizeof(char));
@@ -61,41 +61,14 @@ static void test_interpreter_success_simple(void **state) {
     free(tape);
 }
 
-static void test_interpreter_error_left_bound(void **state) {
-    (void) state;
-    int tape_size = 5;
-    char *tape = (char*)calloc(tape_size, sizeof(char));
-    
-    const char *code = "<+"; 
-    int result = run_interpreter(code, strlen(code), tape, tape_size, 0);
-    
-    assert_int_equal(result, -1); 
-    
-    free(tape);
-}
-
-static void test_interpreter_error_right_bound(void **state) {
-    (void) state;
-    int tape_size = 2;
-    char *tape = (char*)calloc(tape_size, sizeof(char));
-    
-    const char *code = ">>>+"; 
-    int result = run_interpreter(code, strlen(code), tape, tape_size, 0);
-    
-    assert_int_equal(result, -1); 
-    
-    free(tape);
-}
 
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_check_data_correct),
         cmocka_unit_test(test_check_data_unmatched_close),
         cmocka_unit_test(test_check_data_unmatched_open),
-        cmocka_unit_test(test_clear_tape),
-        cmocka_unit_test(test_interpreter_success_simple),
-        cmocka_unit_test(test_interpreter_error_left_bound),
-        cmocka_unit_test(test_interpreter_error_right_bound)
+        cmocka_unit_test(test_clear),
+        cmocka_unit_test(test_interpreter),
     };
     
     return cmocka_run_group_tests(tests, NULL, NULL);
